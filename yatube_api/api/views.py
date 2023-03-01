bbf70ab6168daff292b89b384ac41f75bd542631
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-01 13:44:53 +0300

    Add AuthorOrReadOnly permission

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,14 +1,15 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 
 from posts.models import Post, Comment, Follow
 
 from api.serializers import PostSerializer, CommentSerializer
+from api.permissions import AuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -17,4 +18,4 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (AuthorOrReadOnly,)
