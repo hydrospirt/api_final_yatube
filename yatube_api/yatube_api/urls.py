bac49695520101a7e80c85fb3eff779989c8abae
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-01 12:51:42 +0300

    Add if DEBUG condition

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -14,3 +18,6 @@ urlpatterns = [
     ),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
