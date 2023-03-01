e97a5b27b784a4239b248a52355ffdbda2f6b271
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-01 15:51:51 +0300

    Add new routers

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import PostViewSet, CommentViewSet
+from api.views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
