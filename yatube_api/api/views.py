a4095f53301a87380d3d18114f86779d0e66cb69
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-01 12:53:13 +0300

    Add PostViewSet and CommentViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,18 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from posts.models import Post, Comment, Follow
+
+from api.serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
