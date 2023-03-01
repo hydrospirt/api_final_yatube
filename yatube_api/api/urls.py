d1425df30ab38932c4d6ba34c38226ba919653b8
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-01 14:31:59 +0300

    Fix path CommentViewSet

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,7 +5,7 @@ from api.views import PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
