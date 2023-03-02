2af77486d9955bda212711090298e4740be477ba
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-02 12:51:45 +0300

    Fix permissions

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from posts.models import User, Post, Comment, Follow, Group
@@ -11,7 +11,7 @@ from api.permissions import AuthorOrReadOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, AuthorOrReadOnly)
+    permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -23,7 +23,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, AuthorOrReadOnly)
+    permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
@@ -42,7 +42,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, AuthorOrReadOnly)
+    permission_classes = (AuthorOrReadOnly,)
 
 
 class FollowViewSet(viewsets.ModelViewSet):
@@ -50,9 +50,15 @@ class FollowViewSet(viewsets.ModelViewSet):
     serializer_class = FollowSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ('user',)
-    search_fields = ('^following',)
+    search_fields = ('^following__username',)
     http_method_names = ('get', 'post')
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_queryset(self):
-        return Follow.objects.filter(following=self.request.user)
+        user = get_object_or_404(User, username=self.request.user)
+        return user.follower.all()
+
+
 
