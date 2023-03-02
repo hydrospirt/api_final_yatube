cd6190c49c3d5dc45758ad40dcfba07ae36659f2
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-02 12:50:06 +0300

    Fix has_object_permission

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,9 +5,11 @@ class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return(
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS or request.user
+            and request.user.is_authenticated or request.user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.author == request.user
