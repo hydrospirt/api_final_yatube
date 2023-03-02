472d54c7ad060f83c4af31b20bc318aa986eb776
Эдуард Гумен <hydrospirt@yandex.ru>
2023-03-02 12:50:57 +0300

    Add basenames and jwt paths

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,12 +4,12 @@ from rest_framework import routers
 from api.views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'follow', FollowViewSet)
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'follow', FollowViewSet, basename='follows')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include('djoser.urls.jwt'))
 ]
